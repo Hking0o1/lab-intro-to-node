8bad07a9763fab33c2c47da3975fabe939bbfe54
Himanshu Kulshrestha <Hkulshrestha6@gmail.com>
2024-04-24 10:11:56 +0530

    done

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+    constructor() {
+        this.items = [];
+        this.length = 0;
+    }
 
-  add(item) {}
+    add(item) {
+        this.items.push(item);
+        this.items.sort((a, b) => a - b);
+        this.length = this.items.length;
+    }
 
-  get(pos) {}
+    get(pos) {
+        if (pos < 0 || pos >= this.length) {
+            throw new Error('OutOfBounds');
+        }
+        return this.items[pos];
+    }
+    max() {
+        if (this.length === 0) {
+            throw new Error('EmptySortedList');
+        }
+        return this.items[this.length - 1];
+    }
 
-  max() {}
+    min() {
+        if (this.length === 0) {
+            throw new Error('EmptySortedList');
+        }
+        return Math.min(...this.items);
+    }
 
-  min() {}
+    sum() {
+        if (this.length === 0) {
+            return 0;
+        } else {
+            return this.items.reduce((acc, val) => acc + val, 0);
+        }
+    }
 
-  sum() {}
-
-  avg() {}
+    avg() {
+        if (this.length === 0) {
+            throw new Error('EmptySortedList');
+        } else {
+            return this.sum() / this.length;
+        }
+    }
 }
 
 module.exports = SortedList;
